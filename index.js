70017dd56ac0632e2ce97bc2f49e58c46321011a
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-11 02:06:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function sayHiToGrandma(string) {
   if (string == string.toLowerCase())
   return "I can't hear you!";
 }
-}
+
 
 function sayHiToGrandma(string) {
-  if (string == string.toUpperCase()) {
+  if (string == string.toUpperCase()) 
   return "YES INDEED!";
 }
-}
+

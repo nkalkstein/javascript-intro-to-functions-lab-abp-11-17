14d66745a0927fb12f58da717196342602de2198
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-11 02:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string == string.toLowerCase()) 
+  if (string == string.toLowerCase())
   return "I can't hear you!";
 }
 }
